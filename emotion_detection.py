bb9b891929dc21e53884da2bb3dec2aa4b882dcd
tomander <tlanders@outlook.dk>
2026-03-01 20:52:18 +0100

    emotion_detector

---
+++ emotion_detection.py
@@ -0,0 +1,15 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyse):
+    """Run emotion detector"""
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}  # Set the headers required for the API request
+    myobj = { "raw_document": { "text": text_to_analyse } }  # Create a dictionary with the text to be analyzed
+    response = requests.post(url, json = myobj, headers=header)  # Send a POST request to the API with the text and headers
+    formatted_response = response.json()
+    if response.status_code == 200:
+        text = formatted_response
+    else:
+        text = None
+    return text
